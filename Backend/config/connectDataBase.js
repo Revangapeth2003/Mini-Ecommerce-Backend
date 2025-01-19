7ae20381ac6f0772aa14e84ce6e8881a14868409
Revangapeth2003 <Revangapeth@gmail.com>
2025-01-19 21:08:07 +0530

    change in connect db

--- Backend/config/connectDataBase.js
+++ Backend/config/connectDataBase.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDataBase = () => {
-  mongoose.connect(process.env.DB_URL).then((con) => {
-    console.log("mongoDB is connected in host :" + con.connection.host);
-  });
+  mongoose
+    .connect(process.env.DB_URL)
+    .then((con) => {
+      console.log("mongoDB is connected in host :" + con.connection.host);
+    })
+    .catch((Error) => console.error(Error));
 };
 
 module.exports = connectDataBase;
